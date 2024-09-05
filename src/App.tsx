97c37a3145625fc38be42b46af89d9fb05b668be
cfviotti <caio@viotti.me>
2024-09-05 18:38:35 -0300

    Release Candidate 1
    🫣

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import esiLogo from './assets/esi-logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <img src={esiLogo} className="logo" alt="External Secrets" />
+      <h1>ESI on the block, baby!</h1>
+    </>
+  )
+}
+
+export default App
