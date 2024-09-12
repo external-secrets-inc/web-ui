029e0c3ab87cf4ef044a4a4191b25348ff2fca8e
cfviotti <caio@viotti.me>
2024-09-12 10:02:50 -0300

    Simplify delete dialog

--- src/components/agents/DeleteAgentDialog.tsx
+++ src/components/agents/DeleteAgentDialog.tsx
@@ -1,27 +1,18 @@
 import { Button } from "@/components/ui/button"
 import {
   Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
-import { DialogClose } from "@radix-ui/react-dialog"
-import { Label } from "@radix-ui/react-label"
-import axios from "axios"
-import { Trash2Icon } from "lucide-react"
-import { useEffect, useState } from "react"
 import { DeleteAgentModalContent } from "./DeleteAgentModalContent"
+import { Trash2Icon } from "lucide-react"
 
-export function DeleteAgentDialog({id, onDeleted, variant= "destructive", showIcon = true, showLabel = true}) {
+export function DeleteAgentDialog({id, onDeleted}) {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant={variant} className="cursor-pointer">
-            {showLabel && <Label> Delete</Label>}
-            {showIcon && <Trash2Icon/>}
+        <Button variant="destructive" className="md:mr-auto">
+          <Trash2Icon className="mr-2" />
+          Delete
         </Button>
       </DialogTrigger>
       <DeleteAgentModalContent id={id} onDeleted={onDeleted} />
