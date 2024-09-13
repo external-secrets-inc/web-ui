2b9cd933b29d79a89dad4ac027db123ea435de0f
cfviotti <caio@viotti.me>
2024-09-13 16:38:43 -0300

    Fix auth navigation with new list agentes route

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { ListAgents } from "@/components/agents/ListAgents";
 import Auth from "@/components/Auth";
-import { Home } from "@/components/Home";
 import { NotFound } from "@/components/NotFound";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { Toaster } from '@/components/ui/sonner';
@@ -8,7 +7,7 @@ import RequireAuth from '@auth-kit/react-router/RequireAuth';
 import * as React from "react";
 import AuthProvider from 'react-auth-kit';
 import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
 import './index.css';
 import authStore from "./services/auth/authStore";
 
@@ -16,11 +15,7 @@ import authStore from "./services/auth/authStore";
 const router = createBrowserRouter([
   {
     path: '/',
-    element: (
-      <RequireAuth fallbackPath="/login">
-        <Home />
-      </RequireAuth>
-    ),
+    element: <Navigate to="/agents" replace />,
   },
   {
     path: '/signup',
@@ -32,7 +27,11 @@ const router = createBrowserRouter([
   },
   {
     path: '/agents',
-    element: <ListAgents />,
+    element: (
+      <RequireAuth fallbackPath="/signup">
+        <ListAgents />
+      </RequireAuth>
+    ),
   },
   {
     path: '*',
