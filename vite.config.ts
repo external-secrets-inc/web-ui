2e04e7c73ed0a929796ab14c94bfcd9aa5e7c0c6
cfviotti <caio@viotti.me>
2024-09-19 18:04:09 -0300

    quickfis: Bump vite target environment
    Build was breaking due to usage of top level `await` commited recently

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import path from "path"
-import react from '@vitejs/plugin-react-swc'
+import path from "path";
+import react from '@vitejs/plugin-react-swc';
 import svgr from "vite-plugin-svgr";
-import { defineConfig } from "vite"
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), svgr()],
@@ -13,4 +13,7 @@ export default defineConfig({
   server: {
     port: 1234,
   },
-})
+  build: {
+    target: 'ES2022',
+  },
+});
