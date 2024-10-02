716a145737eda8edba5411ebcb664094d65e34fd
cfviotti <caio@viotti.me>
2024-10-01 23:37:58 -0300

    fix: :lipstick: Fix main block padding

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div className="app-layout">
       <AppTopBar />
-      <main className="container mx-auto text-left flex flex-col py-14">
+      <main className="container mx-auto text-left flex flex-col py-6 md:py-11">
         <Outlet />
       </main>
     </div>
