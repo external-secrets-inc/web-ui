bd255c250174b9b311d62f23264bfde2d12860b8
Parley Martins <parleypachecomartins@gmail.com>
2024-10-04 20:33:10 +0200

    Update css

--- src/components/Auth/ResetPasswordFlow/ResetPasswordFlow.tsx
+++ src/components/Auth/ResetPasswordFlow/ResetPasswordFlow.tsx
@@ -9,9 +9,9 @@ function ResetPasswordFlow({ variant }: ResetPasswordFlowProps) {
   return (
     <>
       <div className="p-2 lg:p-20 min-h-dvh flex flex-col bg-gradient-to-tl from-violet-400/60 to-violet-950">
-        <div className="w-full flex-1 flex flex-col max-w-[800px] mx-auto lg:grid lg:grid-cols-[minmax(30%,60%)_minmax(auto,auto)] rounded-[32px] overflow-hidden bg-background">
+        <div className="w-full flex-1 flex flex-col max-w-[800px] mx-auto lg:grid rounded-[32px] overflow-hidden bg-background">
           <div className="flex items-center justify-center p-10 lg:p-14 flex-1">
-            <div className="mx-auto grid w-[352px] gap-6">
+            <div className="mx-auto grid w-full gap-6">
               <div className="grid gap-2">
                 <h1 className="text-3xl font-bold">{variant === 'forgot' ? 'Forgot your password?' : 'Reset Your Password'}</h1>
                 <p className="text-pretty text-muted-foreground">
