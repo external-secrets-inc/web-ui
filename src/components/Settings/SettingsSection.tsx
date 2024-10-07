f62d34aedfab6cbbc0c75a1079b2ab8d1aa7144a
cfviotti <caio@viotti.me>
2024-10-07 19:03:00 -0300

    fix: :lipstick: fix settings section spacing

--- src/components/Settings/SettingsSection.tsx
+++ src/components/Settings/SettingsSection.tsx
@@ -39,19 +39,21 @@ const SettingsSection: React.FC<SettingsSectionProps> = ({ title, description, f
         <h2 className='text-sm font-semibold'>{title}</h2>
         <h3 className='text-sm text-muted-foreground'>{description}</h3>
       </div>
-      {subsections.map((subsection, index) => (
-        <section key={index} className='grid md:grid-cols-2 gap-4 mb-6'>
-          <div>
-            <h4 className='text-sm font-semibold'>{subsection.title}</h4>
-          </div>
-          <div className="flex flex-col h-full">
-            {subsection.content}
-          </div>
-        </section>
-      ))}
+      <div className='flex flex-col gap-8'>
+        {subsections.map((subsection, index) => (
+          <section key={index} className='grid md:grid-cols-2 gap-6 pb-8 border-b'>
+            <div>
+              <h4 className='text-sm font-semibold'>{subsection.title}</h4>
+            </div>
+            <div className="flex flex-col gap-4 h-full">
+              {subsection.content}
+            </div>
+          </section>
+        ))}
+      </div>
       <form
         id={`${title.toLowerCase()}-form`}
-        className="flex gap-2 justify-end mt-6 py-4 border-t sticky bottom-0 bg-background"
+        className="flex gap-2 justify-end -mt-px py-4 border-t sticky bottom-0 bg-background"
         autoComplete="off"
         onSubmit={form.handleSubmit(handleSettingsSectionFormSubmit)}
       >
