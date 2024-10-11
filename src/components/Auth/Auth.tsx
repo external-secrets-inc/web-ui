8ed42b8672ffe5a45453b064ef7d5aed74267419
cfviotti <caio@viotti.me>
2024-10-11 11:04:05 -0300

    refactor: :iphone: improve mobile responsiveness

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -31,7 +31,7 @@ function Auth({ variant }: AuthProps) {
 
   return (
     <div className="p-2 lg:p-14 xl:p-20 min-h-dvh flex flex-col">
-      <div className="z-10 w-full flex-1 flex flex-col max-w-[1600px] mx-auto lg:grid lg:grid-cols-[minmax(30%,60%)_minmax(auto,auto)] rounded-[32px] overflow-hidden backdrop-brightness-[2.75] dark:backdrop-brightness-75 dark:backdrop-contrast-200 bg-background/50 dark:bg-background/90">
+      <div className="z-10 lg:w-full lg:flex-1 flex flex-col max-w-full lg:max-w-[1600px] m-auto lg:grid lg:grid-cols-[minmax(30%,60%)_minmax(auto,auto)] rounded-[32px] overflow-hidden backdrop-brightness-[2.75] dark:backdrop-brightness-75 dark:backdrop-contrast-200 bg-background/50 dark:bg-background/90">
         <div className="relative contain-content p-8 py-6 sm:py-8 lg:p-20 flex flex-col flex-0 gap-6 items-start justify-between rounded-[inherit]">
           <a
             href={WEBSITE_DOMAIN}
@@ -44,7 +44,7 @@ function Auth({ variant }: AuthProps) {
               alt="External Secrets"
             />
           </a>
-          <h1 className="hidden sm:block text-base sm:text-xl font-bold text-white lg:text-3xl xl:text-5xl lg:max-w-[310px] xl:max-w-[480px]">
+          <h1 className="hidden sm:block text-base font-bold text-white lg:text-3xl xl:text-5xl lg:max-w-[310px] xl:max-w-[480px]">
             Your seamless secrets management journey starts here
           </h1>
           <div
