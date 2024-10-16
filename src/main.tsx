d395ca205fadcb3a1e9e6dcf8fc11ea0b7bd76c0
cfviotti <caio@viotti.me>
2024-10-16 12:39:36 -0300

    feat: :children_crossing: improve agents header copy with more context and docs

--- src/main.tsx
+++ src/main.tsx
@@ -19,6 +19,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { load, page } from './analytics';
 import App from './App';
 import './index.css';
+import { DOCS_DOMAIN } from "@/constants";
 
 const router = createBrowserRouter([
   {
@@ -67,7 +68,12 @@ const router = createBrowserRouter([
           <>
             <AppPageHeader
               title="Your Agents"
-              description="Monitor and create agents for ESO deployments"
+              description={
+                <>
+                  Agents deploy and update ESO Deployments for you<br/>
+                  See our <a href={`${DOCS_DOMAIN}/docs/esi-agent/quickstart`}>Quickstart guide</a> and <a href={`${DOCS_DOMAIN}/docs/esi-for-eso/quickstart`}>Exclusive Features</a> for more details
+                </>
+              }
             />
             <ListAgents />
           </>
