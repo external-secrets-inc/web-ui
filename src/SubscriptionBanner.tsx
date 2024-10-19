850594c5bbed32d9c12a0068354cebb78fca8f49
Bobzitto <soujv91@hotmail.com>
2024-10-19 11:46:02 -0300

    Banner working with fetched user data rather than dummy data. Reworked css to fit mobile version.

--- src/SubscriptionBanner.tsx
+++ src/SubscriptionBanner.tsx
@@ -1,23 +1,38 @@
 import { useEffect, useState } from "react";
 import { Alert, AlertDescription } from "./components/ui/alert";
+import { getSubscriptions } from "./services/subscriptions/subscriptionsService";
+import { toast } from "sonner";
+import { Subscription } from "./types";
 
 function SubscriptionBanner() {
-  // Dummy expiration data (for testing purposes)
-  const dummyExpirationDate = new Date();
-  dummyExpirationDate.setDate(dummyExpirationDate.getDate() + 3); // 3 days from now
-
   const [daysUntilExpiration, setDaysUntilExpiration] = useState(0);
-  const [dismissed, setDismissed] = useState(false); // Track dismissal
+  const [subscriptionId, setSubscriptionId] = useState<string | null>(null);
+  const [dismissed, setDismissed] = useState(false);
   const [threshold, setThreshold] = useState<string | number | null>(null); // Expiration state
   const [isFadingOut, setIsFadingOut] = useState(false);
   const [expirationDateFormatted, setExpirationDateFormatted] =
     useState<string>(""); // Formatted expiration date
 
   useEffect(() => {
-    calculateDaysUntilExpiration(dummyExpirationDate);
+    const fetchSubscription = async () => {
+      try {
+        const subscriptions: Subscription[] = await getSubscriptions();
+        const subscription = subscriptions.find((sub) => sub.id);
+
+        if (subscription) {
+          setSubscriptionId(subscription.id);
+          const expiryDate = new Date(subscription.expiryDate);
+          calculateDaysUntilExpiration(expiryDate); // Implement this function
+        }
+      } catch (error) {
+        toast.error("Failed to load subscription data");
+      }
+    };
+
+    fetchSubscription();
   }, []);
 
-  // calculating days left until sub runs out
+  // calculating days left until subscription runs out
   const calculateDaysUntilExpiration = (expirationDate: Date) => {
     const today = new Date();
     const diffInTime = expirationDate.getTime() - today.getTime();
@@ -61,42 +76,42 @@ function SubscriptionBanner() {
 
   return (
     <div
-      className={`mr-14 ml-14 flex justify-center items-center ${
+      className={`mx-5 md:mx-14 flex justify-center items-center ${
         isFadingOut ? "animate-fade-out" : ""
       }`}
     >
       <Alert
-        variant={threshold === "expired" ? "destructive" : "warning"}
-        className="mt-6 mb-4 w-full max-w-[1380px] flex justify-between items-center pl-4 pr-6"
+        variant={threshold === 0 ? "destructive" : "warning"}
+        className="mt-6 w-full max-w-[1380px] flex justify-between items-center pl-4 pr-6"
       >
         <div className="flex flex-row space-x-4">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
-            className="size-5"
+            className="w-16 h-14 md:w-5 md:h-5"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
             />
           </svg>
 
           <AlertDescription className="text-left">
-            {threshold === "expired"
+            {threshold === 0
               ? "Your subscription has expired! Please renew your subscription."
               : `Your subscription will expire in ${daysUntilExpiration} day(s). Please ensure your next payment is made by ${expirationDateFormatted}.`}
           </AlertDescription>
         </div>
 
-        {threshold !== "expired" && (
+        {threshold !== 0 && (
           <button
             onClick={handleDismiss}
             className={`text-xl font-semibold ${
-              threshold === "expired"
+              threshold === 0
                 ? "hover:text-destructive"
                 : "hover:text-orange-500"
             }`}
