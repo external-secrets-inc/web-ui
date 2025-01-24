ae5baa251e9b135777ed97c77e3147c338fb32bd
Lucas Malacrida Dias <lucasmd99@hotmail.com>
2025-01-24 18:25:42 -0300

    feat: implemented darkmode toggle button (#269)

--- src/components/AppTopBar/AppNavLinks.tsx
+++ src/components/AppTopBar/AppNavLinks.tsx
@@ -1,7 +1,9 @@
 import { SheetClose } from '@/components/ui/sheet';
-import { LucideExternalLink } from 'lucide-react';
+import { LucideExternalLink, LucideMoon, LucideSun } from 'lucide-react';
 import React from 'react';
 import OrgNavLink from './OrgNavLink.tsx';
+import { useTheme } from "@/components/ThemeProvider";
+import { Button } from '@/components/ui/button';
 
 interface NavLinksProps {
   closeSheetOnClick?: boolean;
@@ -9,10 +11,15 @@ interface NavLinksProps {
 
 const NavLinks: React.FC<NavLinksProps> = ({ closeSheetOnClick }) => {
   const docsUrl = `${import.meta.env.VITE_DOCS_DOMAIN}/docs`;
+  const { theme, setTheme } = useTheme();
 
   const LinkWrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => (
     closeSheetOnClick ? <SheetClose asChild>{children}</SheetClose> : <>{children}</>
   );
+
+  const toggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
 
   return (
     <>
@@ -36,11 +43,21 @@ const NavLinks: React.FC<NavLinksProps> = ({ closeSheetOnClick }) => {
         />
       </LinkWrapper>}
       <LinkWrapper>
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={toggleTheme}
+          className="md:ml-auto"
+        >
+          {theme === 'dark' ? <LucideSun className="h-6 w-6" /> : <LucideMoon className="h-6 w-6" />}
+        </Button>
+      </LinkWrapper>
+      <LinkWrapper>
         <a
           href={docsUrl}
           target="_blank"
           rel="noopener noreferrer"
-          className="transition-colors text-muted-foreground hover:text-foreground md:ml-auto flex items-center"
+          className="transition-colors text-muted-foreground hover:text-foreground flex items-center"
         >
           Docs
           <LucideExternalLink className='ml-2' />
