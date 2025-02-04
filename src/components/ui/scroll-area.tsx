1d4112716113ff33797f254013ea24e7a005b6ec
cfviotti <caio@viotti.me>
2025-02-04 04:20:30 -0300

    refactor: :children_crossing: impove scrollarea thumb contrast

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -38,7 +38,7 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border outline outline-1 outline-background/75" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ))
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
