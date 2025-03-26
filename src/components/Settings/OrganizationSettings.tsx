d91fa3cdc6cd7bc32d3c5a289359aa84f818d736
Iuri Severo <nobreiuri@gmail.com>
2025-03-26 17:35:28 -0300

    feat: Add users management data table with add button (WIP)

--- src/components/Settings/OrganizationSettings.tsx
+++ src/components/Settings/OrganizationSettings.tsx
@@ -14,12 +14,22 @@ import { Input } from "@/components/ui/input";
 import { deleteAccount, getAccountData, updateAccountData } from '@/services/account/accountService';
 import { useSignOut } from '@/hooks/useSignOut';
 import { zodResolver } from "@hookform/resolvers/zod";
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { z } from "zod";
 import SettingsSection from './SettingsSection';
 import { Skeleton } from '@/components/ui/skeleton';
+import { LucideEdit, LucideMoreVertical, LucidePlus, LucideTrash2 } from "lucide-react";
+import { DataProvider, DataTable } from "../ui/DataProvider";
+import { createColumnHelper } from "@tanstack/react-table";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu";
+import { FeatureItemDeleteAction } from "../FeatureCollection/FeatureItemDeleteAction";
+import { AUDIT_QUERY_STALE_TIME } from "../audit/Audit.constants";
+import { AxiosError } from "axios";
+import { ApiHttpError } from "@/types";
+import { handleDefaultApiHttpError } from "@/services/servicesHelpers";
+import { Dialog, DialogTrigger } from "../ui/dialog";
 
 const formSchema = z.object({
   contact_email: z.string().email({ message: "Invalid email address" }),
@@ -35,6 +45,24 @@ const deleteFormSchema = (tenantId: string) => z.object({
 
 type FormSchemaType = z.infer<typeof formSchema>;
 type DeleteFormSchemaType = z.infer<ReturnType<typeof deleteFormSchema>>;
+
+interface UsersManagementTableData {
+  id: string;
+  tenantID: string;
+  name: string;
+  email: string;
+  permissions: string[];
+}
+
+interface UsersManagementTableMeta {
+  renderRowActions?: (row: UsersManagementTableData) => React.ReactNode;
+}
+
+export interface UserForm {
+  name: string;
+  email: string;
+  permissions: string[];
+}
 
 const OrganizationSettings: React.FC = () => {
   const signOut = useSignOut();
@@ -87,6 +115,165 @@ const OrganizationSettings: React.FC = () => {
     fetchData();
   }, [form, deleteForm]);
 
+  const columnHelper = createColumnHelper<UsersManagementTableData>();
+
+  const usersManagementColumns = useMemo(() => [
+    columnHelper.accessor('name', {
+      header: 'Name',
+      cell: info => <strong>{info.getValue()}</strong>
+    }),
+    columnHelper.accessor('email', {
+      header: 'Email',
+      cell: info => <strong>{info.getValue()}</strong>
+    }),
+    columnHelper.accessor('permissions', {
+      header: 'Permissions',
+      cell: info => info.getValue()?.join(" ; ") || ""
+    }),
+    columnHelper.display({
+      id: 'actions',
+      cell: props => (
+        <div className='flex justify-end'>
+          {(props.table.options.meta as UsersManagementTableMeta)?.renderRowActions?.(props.row.original)}
+        </div>
+      )
+    })
+  ], [columnHelper]);
+
+  const usersManagementTableMeta: UsersManagementTableMeta = {
+    renderRowActions: (row) => (
+      <div className="flex items-center gap-2">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="ghost"
+              size="icon"
+              onClick={(event) => event.stopPropagation()}
+            >
+              <LucideMoreVertical />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            onClick={(event) => event.stopPropagation()}
+            onCloseAutoFocus={(event) => event.preventDefault()}
+          >
+            <DropdownMenuItem
+              onSelect={(e) => {
+                e.preventDefault();
+                // setSelectedUserId(row.id);
+                // setUserForm({ name: row.name, engine: row.engine, executeOn: row.executeOn, sample: "", rule: isBase64(row.rule) ? atob(row.rule) : row.rule });
+                // setIsAddUserDialogOpen(true);
+              }}
+            >
+              <LucideEdit className="mr-2" />
+              Edit User
+            </DropdownMenuItem>
+            <FeatureItemDeleteAction
+              featureType={"Audit User"}
+              featureID={row.id}
+              featureName={row.name}
+              onDelete={() => { performDelete(row.id) }}
+            >
+              <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
+                <LucideTrash2 className="mr-2" />
+                Delete User
+              </DropdownMenuItem>
+            </FeatureItemDeleteAction>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    )
+  };
+
+  const [isAddUserDialogOpen, setIsAddUserDialogOpen] = useState(false);
+  const defaultUserFormValues = {
+    name: "",
+    email: "",
+    permissions: [],
+  };
+  const [userForm, setUserForm] = useState<UserForm>(defaultUserFormValues);
+  const [selectedUserId, setSelectedUserId] = useState<string>("");
+
+  const {
+    data: usersData,
+    refetch: usersRefetch,
+    isLoading: isLoadingPolicies,
+    isError: isErrorUsers,
+    isRefetchError: isRefetchErrorUsers,
+    error: usersError
+  } = useGetUsers(false, accountData?.tenant_id, {
+    staleTime: AUDIT_QUERY_STALE_TIME,
+  });
+
+  const users = useMemo(() => {
+    if (!usersData) return []
+
+    // Transform the API response to include the required id dataProvider field
+    return usersData.map(user => ({
+      ...user,
+      id: user.id,
+    }));
+  }, [usersData]);
+
+  const { mutate: createUser } = useCreateUser(false, {
+    onError: (error: AxiosError<ApiHttpError>) => handleDefaultApiHttpError(error, "Error while trying to create User"),
+    onSuccess: () => {
+      usersRefetch();
+      toast.success("User created successfully")
+    },
+  });
+
+  const { mutate: editUser } = useEditUser(false, {
+    onError: (error: AxiosError<ApiHttpError>) => handleDefaultApiHttpError(error, "Error while trying to edit User"),
+    onSuccess: () => {
+      usersRefetch();
+      toast.success("User edited successfully")
+    },
+  });
+
+  const { mutate: deleteUser } = useDeleteUser(false, {
+    onError: (error: AxiosError<ApiHttpError>) => handleDefaultApiHttpError(error, "Error while trying to delete User"),
+    onSuccess: () => {
+      usersRefetch();
+      toast.success("User deleted successfully")
+    },
+  });
+
+  const performCreate = (payload: CreateUserPayload) => {
+    payload.tenantID = accountData?.tenant_id;
+    createUser(payload)
+  };
+
+  const performEdit = (editPayload: EditUserPayload) => {
+    editUser(editPayload);
+  };
+
+  const performDelete = (policyID: string) => {
+    deleteUser({ id: policyID });
+  };
+
+  useEffect(() => {
+    if (!(usersError || isRefetchErrorUsers)) return;
+    handleDefaultApiHttpError(usersError, "Error while fetching listener Audit data");
+  }, [usersError, isErrorUsers, isRefetchErrorUsers]);
+
+  const handleAddUserDialogOpenChange = (isOpen: boolean) => {
+    setIsAddUserDialogOpen(isOpen);
+    setUserForm(defaultUserFormValues);
+    setSelectedUserId("");
+  };
+
+  const handleAddUserSubmit = (payload: CreateUserPayload) => {
+    if (selectedUserId) {
+      const { name, executeOn, engine, rule } = { ...payload };
+      const editPayload = { policyID: selectedUserId, payload: { name, executeOn, engine, rule } };
+      performEdit(editPayload);
+    } else {
+      performCreate(payload);
+    }
+    handleAddUserDialogOpenChange(false);
+  }
+
   async function handleSaveSection(values: FormSchemaType) {
     const dataToSend = {
       ...values,
@@ -121,7 +308,7 @@ const OrganizationSettings: React.FC = () => {
         <>
           <div className='space-y-2'>
             <FormLabel>Tenant ID</FormLabel>
-            { accountData
+            {accountData
               ? <p className="text-sm text-muted-foreground">{accountData?.tenant_id}</p>
               : <Skeleton className='h-5 w-[stretch] max-w-48' />
             }
@@ -182,6 +369,40 @@ const OrganizationSettings: React.FC = () => {
               </FormItem>
             )}
           />
+        </>
+      ),
+    },
+    {
+      title: 'Contact Information',
+      content: (
+        <>
+          <DataProvider
+            data={users}
+            columns={usersManagementColumns}
+            initialSort={{ id: 'name', desc: false }}
+            isLoading={isLoadingPolicies}
+          >
+            <DataTable
+              meta={usersManagementTableMeta}
+            />
+          </DataProvider>
+          <Dialog open={isAddUserDialogOpen} onOpenChange={handleAddUserDialogOpenChange}>
+            <DialogTrigger asChild>
+              <Button
+                className="self-end"
+                variant="outline"
+              >
+                <LucidePlus />
+                Add User
+              </Button>
+            </DialogTrigger>
+            <UserDialogForm
+              selectedUserId={selectedUserId}
+              userForm={userForm}
+              onSubmit={handleAddUserSubmit}
+              onCancel={() => { handleAddUserDialogOpenChange(false) }}
+            />
+          </Dialog>
         </>
       ),
     },
