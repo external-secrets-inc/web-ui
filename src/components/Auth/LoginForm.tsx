38974960d9bef2ad6d5fbc781066cb8f65a3aef1
msfernandes <matheus.souza.fernandes@gmail.com>
2025-04-17 03:37:09 -0300

    feat: update login and signup form to use new queries

--- src/components/Auth/LoginForm.tsx
+++ src/components/Auth/LoginForm.tsx
@@ -5,11 +5,13 @@ import useSignIn from "react-auth-kit/hooks/useSignIn";
 import { Link, useNavigate } from "react-router-dom";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { isAxiosError } from "axios";
+import { AxiosError, isAxiosError } from "axios";
 import { LoginOrganizationURLStep } from "./LoginOrganizationURLStep";
 import { LoginCredentialsStep } from "./LoginCredentialsStep";
 import zValidations from "./fields/zValidations";
-import { loginAndIdentifyUser } from "@/services/auth/authHelpers";
+import { ApiHttpError } from "@/types";
+import { LoginAndIdentifyParams } from "@/services/auth/Auth.interfaces";
+import useLoginAndIdentifyUser from "@/services/auth/mutations/useLoginAndIdentifyUser";
 
 const LoginOrganizationURLSchema = z.object({
   organizationURL: zValidations.organizationURL
@@ -32,10 +34,34 @@ interface LoginFormProps {
 
 function LoginForm({ onStepChange, onOrganizationURLChange }: LoginFormProps) {
   const [step, setStep] = useState<Step>("organizationURL");
-  const [loading, setLoading] = useState<boolean>(false);
   const [formError, setFormError] = useState<string | null>(null);
   const authKitSignIn = useSignIn();
   const navigate = useNavigate();
+
+  const { mutate: loginAndIdentifyUser, isPending: isLoginPending } = useLoginAndIdentifyUser({
+    onError: (error: AxiosError<ApiHttpError>) => {
+      const stockError = "Signup failed. Please try again.";
+      if (isAxiosError(error)) {
+        const responseError = error.response?.data?.errors?.error;
+        if (responseError?.includes("invalid username/password")) {
+          return setFormError("Invalid login credentials");
+        }
+
+        if (responseError?.includes("invalid tenant")) {
+          setStep("organizationURL");
+          onStepChange("organizationURL");
+          return formMethods.setError("organizationURL", { type: "manual", message: "Invalid Organization URL" });
+        }
+      }
+
+      console.error("Non-Axios error:", error);
+      setFormError(stockError);
+    },
+    onSuccess: (_, variables: LoginAndIdentifyParams) => {
+      trackSignedIn(variables.tenantSlug);
+      return navigate(`/${variables.tenantSlug}/agents`);
+    },
+  })
 
   const formMethods = useForm<LoginData>({
     resolver: zodResolver(step === "organizationURL" ? LoginOrganizationURLSchema : LoginCredentialsSchema),
@@ -55,52 +81,16 @@ function LoginForm({ onStepChange, onOrganizationURLChange }: LoginFormProps) {
   };
 
   const handleCredentialsStepSubmit = async (data: LoginCredentialsData) => {
-    setLoading(true);
     formMethods.clearErrors();
     setFormError(null);
-    const stockError = "Login failed. Please try again.";
 
-    const handleLoginErrors = (error: any) => { // eslint-disable-line @typescript-eslint/no-explicit-any
-      if (isAxiosError(error)) {
-        const responseError = error.response?.data?.errors?.body;
-
-        if (responseError?.includes("invalid username/password")) {
-          return setFormError("Invalid login credentials");
-        }
-
-        if (responseError?.includes("invalid tenant")) {
-          setStep("organizationURL");
-          onStepChange("organizationURL");
-          return formMethods.setError("organizationURL", { type: "manual", message: "Invalid Organization URL" });
-        }
-      }
-
-      console.error("Non-Axios error:", error);
-      setFormError(stockError);
-    };
-
-    try {
-      const formData = formMethods.getValues();
-      const hasSignedIn = await loginAndIdentifyUser({
-        email: data.email!,
-        password: data.password!,
-        tenantSlug: formData.organizationURL!,
-        authKitSignIn,
-      });
-
-      trackLoginStepCompleted(2);
-
-      if (hasSignedIn) {
-        trackSignedIn(formData.organizationURL!);
-        return navigate(`/${formData.organizationURL}/agents`);
-      }
-
-      handleLoginErrors(new Error(stockError));
-    } catch (err) {
-      handleLoginErrors(err);
-    } finally {
-      setLoading(false);
-    }
+    const formData = formMethods.getValues();
+    loginAndIdentifyUser({
+      email: data.email!,
+      password: data.password!,
+      tenantSlug: formData.organizationURL!,
+      authKitSignIn,
+    });
   };
 
   const handleBack = () => {
@@ -120,7 +110,7 @@ function LoginForm({ onStepChange, onOrganizationURLChange }: LoginFormProps) {
           <LoginCredentialsStep
             onSubmit={formMethods.handleSubmit(handleCredentialsStepSubmit)}
             onBack={handleBack}
-            loading={loading}
+            loading={isLoginPending}
           />
         )}
         {formError ? <div className="text-destructive">{formError}</div> : null}
@@ -132,7 +122,7 @@ function LoginForm({ onStepChange, onOrganizationURLChange }: LoginFormProps) {
           to="/signup"
           className={`
             underline text-foreground text-nowrap
-            ${loading ? 'pointer-events-none text-muted-foreground/50 no-underline' : ''}
+            ${isLoginPending ? 'pointer-events-none text-muted-foreground/50 no-underline' : ''}
           `}
         >
           Sign up for one
