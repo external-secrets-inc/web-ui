065fbe87e090d373c432d3cadb698fd28045848c
cfviotti <caio@viotti.me>
2025-04-17 11:38:27 -0300

    feat: :sparkles: add destinations queries and mutatiosn and update policies query

---
+++ src/services/audit/mutations/useDeleteDestination.ts
@@ -0,0 +1,37 @@
+import { UseMutationOptions, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getAuthHeaders } from "@/services/auth/authHelpers";
+import axiosInstance from "@/services/axiosConfig";
+import { ApiHttpError } from "@/types";
+import { AxiosError } from "axios";
+
+interface DeleteDestinationVariables {
+  destinationID: string;
+}
+
+const deleteDestination = async (
+  { destinationID }: DeleteDestinationVariables,
+): Promise<void> => {
+  const headers = await getAuthHeaders();
+  await axiosInstance.delete(
+    `/api/destinations/${destinationID}`,
+    {
+      headers,
+      backend: 'AUDIT_POC'
+    }
+  );
+};
+
+export default function useDeleteDestination(
+  options?: Omit<UseMutationOptions<void, AxiosError<ApiHttpError>, DeleteDestinationVariables>, "mutationFn">
+) {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: deleteDestination,
+    onSuccess: (data, variables, context) => {
+      queryClient.invalidateQueries({ queryKey: ["audit", "destinations"] });
+      options?.onSuccess?.(data, variables, context);
+    },
+    ...options,
+  });
+}
