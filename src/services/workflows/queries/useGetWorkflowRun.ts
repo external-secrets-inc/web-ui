89e04b7f775d36018d1c9a4e61d39defe512548f
Iuri Severo <nobreiuri@gmail.com>
2025-06-25 17:14:29 -0300

    feat: add workflow run graph page (WIP)

---
+++ src/services/workflows/queries/useGetWorkflowRun.ts
@@ -0,0 +1,29 @@
+import { UseQueryOptions, useQuery } from "@tanstack/react-query";
+import { getAuthHeaders } from "@/services/auth/authHelpers";
+import axiosInstance from "@/services/axiosConfig";
+import { ApiHttpError } from "@/types";
+import { AxiosError } from "axios";
+import { GetWorkflowRunPayload, WorkflowTemplateTableData } from "@/components/workflows/Workflows/Workflows.interfaces";
+
+const getWorkflowRun = async (signal: AbortSignal, payload: GetWorkflowRunPayload): Promise<WorkflowTemplateTableData[]> => {
+  const headers = await getAuthHeaders();
+  const response = await axiosInstance.get(`/api/v1/workflows/${payload.namespace}/${payload.name}`, {
+    headers,
+    signal,
+    backend: 'ESO_SERVER'
+  });
+  return response.data;
+};
+
+const useGetWorkflowRun = (
+  payload: GetWorkflowRunPayload,
+  options?: Omit<UseQueryOptions<WorkflowTemplateTableData[], AxiosError<ApiHttpError>>, 'queryKey' | 'queryFn'>
+) => {
+  return useQuery({
+    queryKey: ["workflows", "useGetWorkflowRun"],
+    queryFn: ({ signal }) => getWorkflowRun(signal, payload),
+    ...options,
+  });
+};
+
+export default useGetWorkflowRun;
