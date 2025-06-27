d21c163627409f0548c6bcb7e68bc0558f8d8880
Iuri Severo <nobreiuri@gmail.com>
2025-06-27 12:07:32 -0300

    fix: lint

--- src/components/workflows/Workflows/WorkflowTemplateDataTable.tsx
+++ src/components/workflows/Workflows/WorkflowTemplateDataTable.tsx
@@ -91,7 +91,7 @@ export function WorkflowTemplateDataTable() {
           ),
         }),
       ]),
-    []
+    [getOrgLink, navigate]
   );
 
   const workflowTemplateTableMeta: WorkflowTemplateTableMeta = {
