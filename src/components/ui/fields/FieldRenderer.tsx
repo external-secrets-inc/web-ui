03e1ba70d026001189569f908594518f3e67a886
Iuri Severo <nobreiuri@gmail.com>
2025-07-30 16:37:58 -0300

    fix: improve formsValue prop driling

--- src/components/ui/fields/FieldRenderer.tsx
+++ src/components/ui/fields/FieldRenderer.tsx
@@ -18,10 +18,9 @@ import { OptionUtils } from "@/components/EsiSchemaForm/EsiSchemaForm.utils";
 
 export interface FieldRendererProps {
   field: UISchemaField;
-  formValues: Record<string, unknown>
 }
 
-export function FieldRenderer({ field, formValues }: FieldRendererProps) {
+export function FieldRenderer({ field }: FieldRendererProps) {
   const baseProps = {
     name: field.id,
     label: field.label,
@@ -63,7 +62,7 @@ export function FieldRenderer({ field, formValues }: FieldRendererProps) {
 
       // if oneOf has no href, it's for sub-schema selection handled by FieldOneOf
       if (field.oneOf && field.oneOf.length > 0) {
-        return <FieldOneOf {...baseProps} field={field} formValues={formValues} />;
+        return <FieldOneOf {...baseProps} field={field} />;
       }
 
       // Otherwise, it's a standard select with static options
@@ -120,19 +119,17 @@ export function FieldRenderer({ field, formValues }: FieldRendererProps) {
           {...baseProps}
           field={field}
           defaultValue={field.default as unknown[]}
-          formValues={formValues}
         />
       );
 
     case "object":
       // Handle oneOf constraint for object fields TODO[cfviotti]: This is supposedly for legacy schemas. It will always be for `select` fields now instead.
       if (field.oneOf && field.oneOf.length > 0) {
-        return <FieldOneOf {...baseProps} field={field} formValues={formValues} />;
+        return <FieldOneOf {...baseProps} field={field} />;
       }
       return (
         <FieldObject
           {...baseProps}
-          formValues={formValues}
           field={field}
           defaultValue={field.default as Record<string, unknown>}
         />
@@ -142,7 +139,7 @@ export function FieldRenderer({ field, formValues }: FieldRendererProps) {
       return <FieldJson {...baseProps} defaultValue={field.default} />;
 
     case "one-of":
-      return <FieldOneOf {...baseProps} field={field} formValues={formValues} />;
+      return <FieldOneOf {...baseProps} field={field} />;
 
     case "secret-selector":
       return <FieldSecretSelect {...baseProps} />;
