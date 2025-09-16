a6e905becf9b9b7371da61fc8b7251cc0d1a063f
Iuri Severo <nobreiuri@gmail.com>
2025-09-16 11:12:15 -0300

    fix: update consumers status map (#507)

--- src/components/workflows/Consumers/ConsumerStatusBadge.tsx
+++ src/components/workflows/Consumers/ConsumerStatusBadge.tsx
@@ -1,43 +1,26 @@
-import { useMemo } from "react";
 import { Status, StatusMap } from "../Common.interfaces";
 import { StatusBadge } from "../StatusBadge";
 
 const CONSUMER_STATUS_MAP: StatusMap = {
-  PendingUpdate: {
+  False: {
     variant: "warning",
     display: (s) => s.reason ?? "Pending Update",
     message: (s) => s.message ?? "Locations out to date",
   },
-  UsingLatestVersion: {
+  True: {
     variant: "success",
     display: (s) => s.reason ?? "Ready",
     message: (s) => s.message ?? "Locations up to date",
   },
 };
 
-function processStatus(status?: Status) : Status | undefined {
-  if(!status) {
-    return undefined;
-  }
-
-  if(status.reason === "LocationsOutOfDate") {
-    status.status = "PendingUpdate"
-  } else if(status.reason === "LocationsUpToDate") {
-    status.status = "UsingLatestVersion"
-  }
-
-  return status;
-}
-
 interface ConsumerStatusBadgeProps {
   statusData?: Status;
 }
 export function ConsumerStatusBadge({ statusData }: ConsumerStatusBadgeProps) {
-  const processedStatus = useMemo(() => processStatus(statusData), [statusData]);
-
   return (
     <StatusBadge
-      statusData={processedStatus}
+      statusData={ statusData }
       map={CONSUMER_STATUS_MAP}
       unknownMessage="No status was retrieved from this consumer"
       formatFunction={ (msg?) => msg?.replace(/:/g, ":\n") }
